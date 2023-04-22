15f008cf5244ff80a39e5fa84f75d1999a296460
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-04-22 18:58:21 +0800

    respond to $hello and $greet

---
+++ Day03/ex04/response.py
@@ -0,0 +1,40 @@
+import discord
+import dotenv
+from pathlib import Path
+import os
+import sys
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Load .env file
+dotenv_path = Path('../../.env')
+dotenv.load_dotenv(dotenv_path=dotenv_path)
+
+# Get discord bot token
+TOKEN = os.getenv('TOKEN')
+if TOKEN is None:
+	print("TOKEN does not exist in environment!", file=sys.stderr)
+	sys.exit()
+
+
+# Run discord bot
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_connect():
+	print("Bot connected to server!")
+
+@client.event
+async def on_ready():
+	print("Logged in as", client.user)
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+	
+	if message.content == '$hello':
+		await message.reply("Hello World!")
+
+client.run(TOKEN)
